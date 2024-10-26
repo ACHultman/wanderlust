337e96e199a056da950b8d57d20474556c87a881
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2024-10-26 11:48:45 -0700

    lint message component

--- components/Chat/MessageList/Message.tsx
+++ components/Chat/MessageList/Message.tsx
@@ -7,21 +7,19 @@ type Props = {
   message: MessageType;
 };
 
-const Message = memo(({ message }: Props) => {
-  return (
-    <Group wrap="nowrap" align="flex-start" grow>
-      <Box>
-        <IconCompass
-          size={32}
-          style={{ maxWidth: '32px' }}
-          visibility={message.role === 'assistant' ? 'visible' : 'hidden'}
-        />
-      </Box>
-      <Text size="xl" maw="100%">
-        {message.content ?? ''}
-      </Text>
-    </Group>
-  );
-});
+const Message = memo(({ message }: Props) => (
+  <Group wrap="nowrap" align="flex-start" grow>
+    <Box>
+      <IconCompass
+        size={32}
+        style={{ maxWidth: '32px' }}
+        visibility={message.role === 'assistant' ? 'visible' : 'hidden'}
+      />
+    </Box>
+    <Text size="xl" maw="100%">
+      {message.content ?? ''}
+    </Text>
+  </Group>
+));
 
 export default Message;
